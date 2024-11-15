426d20743d6fbbd8adf4d283789657f2fdee240f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-15 14:44:12 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// --- Imports and Configuration ---
+require("dotenv").config();
+const express = require("express");
+const path = require("node:path");
+const app = express();
+const {setupDatabase} = require('./db/dbSetup')
+
+// --- Route Imports ---
+const indexRouter = require('./routes/indexRouter')
+
+// --- View Engine Setup ---
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+// --- Middleware Setup ---
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// --- Routes ---
+app.use('/', indexRouter)
+
+// --- Server Configuration and Startup ---
+const PORT = parseInt(process.env.USE_PORT, 10) || 3000;
+async function startServer() {
+    try {
+        await setupDatabase();
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+            console.log(`Visit: http://localhost:${PORT}/`);
+        });
+    } catch (err) {
+        console.error('Failed to start server:', err);
+        process.exit(1);
+    }
+}
+
+startServer();
---
+++ db/dbSetup.js
@@ -0,0 +1,47 @@
+const pool = require('./pool');
+
+setupQuery = `
+CREATE TABLE users (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR( 255 ) NOT NULL,
+    last_name VARCHAR( 255 ) NOT NULL,
+    username VARCHAR( 255 ) NOT NULL UNIQUE,
+    password VARCHAR ( 255 ) NOT NULL,
+    is_member BOOLEAN DEFAULT false
+);
+
+CREATE TABLE messages (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    message TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+`
+
+checkTables = `
+SELECT COUNT(*) = 2 as both_exist
+FROM information_schema.tables 
+WHERE table_name IN ('users', 'messages');
+`
+
+async function setupDatabase() {
+    console.log("Setting up database...");
+    
+    try {
+      const res = await pool.query(checkTables)
+      const tablesExist = res.rows[0].both_exist
+      if (!tablesExist) {
+        console.log("The tables do not exist. Creating...");
+        await pool.query(setupQuery);
+        console.log("Tables created successfully.");
+      } else {
+        console.log("Tables already exists. Skipping setup.");
+      }
+    } catch (err) {
+        console.error('Error setting up database:', err);
+        throw err;
+    }
+  }
+  
+module.exports = { setupDatabase };
