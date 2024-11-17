ee88e0b77e2c226187ef7e7915c25c20621b3deb
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-17 15:20:36 -0500

    add: message query and new message page

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,10 @@ async function addUser(user) {
     await pool.query('INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)', [firstName, lastName, username, password])
 }
 
+async function getAllMessages() {
+    return await db.query('SELECT * FROM messages')
+}
+
 module.exports = {
     addUser,
 }
