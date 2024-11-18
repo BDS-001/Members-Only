ace8ade44bd7ceb64fbc61f7772a7fda394410fa
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-18 16:05:24 -0500

    update: authenticat user directs to login page, authenitcate admin directs to homepage

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,8 +1,15 @@
-function requireAuth(req, res, next) {
+function requireAuthSignedIn(req, res, next) {
     if (!req.user) {
+        return res.redirect('/login')
+    }
+    next()
+}
+
+function requireAuthAdmin(req, res, next) {
+    if (!req?.user?.is_admin) {
         return res.redirect('/')
     }
     next()
 }
 
-module.exports = requireAuth
+module.exports = {requireAuthSignedIn, requireAuthAdmin}
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,12 +4,17 @@ const messageController = require('../controllers/messageController')
 const requireAuth = require('../middleware/requireAuth')
 
 router.get("/", userController.getHomepage);
+
 router.get("/sign-up", userController.getUserSignUp);
 router.post("/sign-up", userController.postUserSignUp);
+
 router.get('/login', userController.getLoginPage)
 router.post("/login", userController.postLogIn);
 router.get("/log-out", userController.getLogOut);
-router.get('/new-message', requireAuth, messageController.getNewMessage)
-router.post('/new-message', requireAuth, messageController.validateMessage, messageController.postNewMessage)
+
+router.get('/new-message', requireAuth.requireAuthSignedIn, messageController.getNewMessage)
+router.post('/new-message', requireAuth.requireAuthSignedIn, messageController.validateMessage, messageController.postNewMessage)
+
+router.post('/delete-message/:id', requireAuth.requireAuthAdmin, messageController.postDeleteMessage)
 
 module.exports = router
